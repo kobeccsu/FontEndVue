c622c90ec8762fe35d4d225acc551a0fd765291f
周磊 <zhoulei@EPBYGROW0055.grodno.epam.com>
2018-08-10 20:40:39 +0800

    component reference component

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -16,16 +16,23 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+      <Header></Header>
   </div>
+
 </template>
 
 <script>
+import Header from "./Header.vue"
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Lei'
     }
+  },
+  components:{
+    Header
   }
 }
 </script>
---
+++ my-vue-app/src/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Here I wanna to Search Something</h1>
+        <input type="text" placeholder="Search...">
+        <button>Search</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+
+</style>
+
