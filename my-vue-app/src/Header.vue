a14b930240c07ca824fdc979d4b33bd94e90eb91
周磊 <zhoulei@EPRUPETW9358.PETERSBURG.epam.com>
2018-08-16 22:58:26 +0800

    event

--- my-vue-app/src/Header.vue
+++ my-vue-app/src/Header.vue
@@ -1,18 +1,29 @@
 <template>
     <div>
         <h1>Here I wanna to Search Something</h1>
-        <input type="text" :placeholder="msg">
-        <button>Search</button>
-        <span v-if="cool">Large</span>
+        <input type="text" :placeholder="msg" v-model="message" @keyup.13="gotoSubmit">
+        <button v-on:click="search">Search</button>
+        <span v-if="cool">{{message}}</span>
     </div>
 </template>
 
 <script>
 export default {
+    props:{},
     data () {
         return {
+            counter: 0,
             msg : 'Search...',
-            cool : false
+            cool : true,
+            message:''
+        }
+    },
+    methods:{
+        search : function(){
+            this.message = 'click ' + (this.counter++) + ' times';
+        },
+        gotoSubmit:function(){
+            this.message = 'Submited!';
         }
     }
 }
