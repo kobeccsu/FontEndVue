a2d4fc2fdbcc29d5e10202ca7fc226b945e69d99
PRINCETON\Lei_Zhou <Lei_Zhou@epam.com>
2018-09-04 10:56:20 +0800

    add some test

--- my-vue-app/src/ulList.vue
+++ my-vue-app/src/ulList.vue
@@ -1,13 +1,14 @@
 <template>
-    <li v-bind:data-group="group">{{todo.text}}</li>
+    <li v-bind:data-group="group" v-bind:data-name="a">{{todo.text}}</li>
 </template>
 
 <script>
+    import {a, b} from '../StaticData'
     export default{
         props : ['todo', 'group'],
         data(){
             return{
-                
+                a: b
             }
         }
     }
