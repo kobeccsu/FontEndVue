a83754f4537cab831f98268f6b8a48d70ffe7ca6
周磊 <zhoulei@EPBYMINW0212.minsk.epam.com>
2018-10-16 22:20:01 +0800

    nested child

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-const Foo = { template: "<div>Foo component</div>" };
+const Foo = { template: "<div>Foo component<router-link to='/foo/ch1'>Ch1</router-link><router-view></router-view></div>" };
 const Bar = { template: "<div>Bar component</div>" };
+
+const Ch1 = { template: "<div>Ch1 Component</div>" };
+const Ch2 = { template: '<div>Ch2 Component</div>' };
 
 const User = {
   template: "<div>User {{$route.params.username}}</div>",
@@ -16,7 +19,9 @@ const User = {
 };
 
 const routes = [
-  { path: '/foo', component: Foo }, 
+  { path: '/foo', component: Foo, children:[
+      { path:'ch1', component: Ch1 }
+  ]}, 
   { path: '/bar', component: Bar },
   { path: '/user/:username', component: User }
 ];
