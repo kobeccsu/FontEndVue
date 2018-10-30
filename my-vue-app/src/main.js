064de011c292ca6c9abbb4601a8a5b3b1ee2018d
PRINCETON\Lei_Zhou <Lei_Zhou@epam.com>
2018-10-30 09:59:09 +0800

    all

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -3,14 +3,15 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-const Foo = { template: "<div>Foo component<router-link to='/foo/ch1'>Ch1</router-link><router-view></router-view><router-view name='ch2'></router-view></div>" };
+const Foo = { template: "<div>Foo component<router-link to='/foo/ju'>Ch1</router-link><router-view></router-view><router-view name='ch2'></router-view></div>" };
 const Bar = { template: "<div>Bar component</div>" };
 
 const Ch1 = { template: "<div>Ch1 Component</div>" };
 const Ch2 = { template: '<div>Ch2 Component</div>' };
 
 const User = {
-  template: "<div>User {{$route.params.username}}</div>",
+  props: ['username'],
+  template: "<div>User {{ username }}</div>",
   watch:{
     '$route' (to, from){
       console.log('from ' + from.params.username + ' to ' + to.params.username);
@@ -23,10 +24,10 @@ const routes = [
     path: '/foo', 
     component: Foo, 
     children:[
-      { path:'ch1', components: {default: Ch1, ch2: Ch2} }
+      { path:'ch1', components: { default: Ch1, ch2: Ch2 }, redirect:'/bar', alias:'ju'}
     ]}, 
   { path: '/bar', component: Bar },
-  { path: '/user/:username', name:'user', component: User }
+  { path: '/user/:username', name:'user', component: User, props: true }
 ];
 
 var router = new VueRouter({ routes });
